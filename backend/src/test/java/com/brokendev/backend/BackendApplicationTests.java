ea7616bfe45e603ef122ce0224eea26e01c09ea5
brokendeveloper <fluccas09@gmail.com>
2025-03-25 00:42:27 -0300

    refactor(main): update to initial project model

---
+++ backend/src/test/java/com/brokendev/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brokendev.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
