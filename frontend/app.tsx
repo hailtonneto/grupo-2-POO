0927170423d756e84ce3112ff36c24b32ede1f3b
Pedro Fernandes <edpedro12345@gmail.com>
2025-04-09 11:03:52 -0300

    feat: create Home page

--- frontend/app.tsx
+++ frontend/app.tsx
@@ -3,7 +3,7 @@ import { NavigationContainer } from "@react-navigation/native"
 import { createStackNavigator } from "@react-navigation/stack"
 import LoginScreen from "./screens/LoginScreen"
 import SignUpScreen from "./screens/SignUpScreen"
-
+import HomeScreen from "./screens/HomeScreen"
 const Stack = createStackNavigator()
 
 export default function App() {
@@ -12,6 +12,7 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
         <Stack.Screen name="SignUp" component={SignUpScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
       </Stack.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
--- frontend/components/logo.jsx
+++ frontend/components/logo.jsx
@@ -1,12 +1,23 @@
-import { View, Text, StyleSheet } from "react-native"
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { useFonts, Kadwa_400Regular, Kadwa_700Bold } from "@expo-google-fonts/kadwa";
 
-export default function Logo() {
+export default function Logo({ styleCesar, styleBank }) {
+  const [fontsLoaded] = useFonts({
+    "Kadwa-Regular": Kadwa_400Regular,
+    "Kadwa-Bold": Kadwa_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.logoContainer}>
-      <Text style={styles.cesarText}>cesar</Text>
-      <Text style={styles.bankText}>bank</Text>
+      <Text style={[styles.cesarText, styleCesar]}>cesar</Text>
+      <Text style={[styles.bankText, styleBank]}>bank</Text>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -15,15 +26,13 @@ const styles = StyleSheet.create({
   },
   cesarText: {
     color: "#42a5f5",
-    fontWeight: "bold",
-    fontSize: 53,
-    fontFamily: "InterRegular",
+    fontSize: 56,
+    fontFamily: "Kadwa-Bold",
   },
   bankText: {
     color: "#42a5f5",
-    fontWeight: "bold",
-    fontSize: 36,
-    marginTop: -26,
-    fontFamily: "InterBold",
+    fontSize: 38,
+    marginTop: -55,
+    fontFamily: "Kadwa-Bold",
   },
-})
+});
